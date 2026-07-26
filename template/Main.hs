fb271f17f3596f88f6c84d2c26464b6aa28570fa
kimura.coki <kimura.coki@gmail.com>
2026-07-26 17:29:54 +0900

    Upd template

--- template/Main.hs
+++ template/Main.hs
@@ -5,17 +5,24 @@ import qualified Data.ByteString.Char8 as BS
 
 main :: IO ()
 main = undefined
--- 入力の読み方:
---   n      <- readInt <$> BS.getLine                   -- 1 行に整数 1 つ
---   [a, b] <- ints <$> BS.getLine                      -- 1 行に整数が複数
---   as     <- ints <$> BS.getLine                      -- 同上、個数が可変
---   xss    <- map ints . BS.lines <$> BS.getContents   -- 残り全部を行ごとに
+-- 入力:
+--   n   <- getInt                                    -- 1 行に整数 1 つ
+--   as  <- getInts                                   -- 1 行に整数が複数
+--   xss <- map ints . BS.lines <$> BS.getContents    -- 残り全部を行ごとに
+-- 出力:
+--   print x         putStrLn s        mapM_ print xs        putInts xs
+
+getInt :: IO Int
+getInt = readInt <$> BS.getLine
+
+getInts :: IO [Int]
+getInts = ints <$> BS.getLine
+
+putInts :: [Int] -> IO ()
+putInts = BS.putStrLn . BS.unwords . map (BS.pack . show)
 
 readInt :: BS.ByteString -> Int
 readInt = fst . fromJust . BS.readInt
 
 ints :: BS.ByteString -> [Int]
 ints = map readInt . BS.words
-
-showInts :: [Int] -> BS.ByteString
-showInts = BS.unwords . map (BS.pack . show)
